e087b4adca3604ab4d7cbccbb539280e4d215a1d
PebblesII <shobell70@gmail.com>
2024-10-02 10:40:09 -0300

    Update

--- True or false.py
+++ True or false.py
@@ -1,40 +1,62 @@
 import time
 import sys
+import os
+
+os.system('cls||echo -e \\\\033c')
+
+def Loading(timer):
+    print("\n")
+    for I in range(timer):
+        print(".", end="")
+        time.sleep(1)
+    print("\n")
 
 #Boolean
+Ligado = False
 
-Habilitado = True
+print("Ligar O Computador?")
+print("    Y        N     ")
+A = input("===    ")
+if A == "Y":
+    Ligado = True
+elif A == "N":
+    Ligado = False
+    print("Turning off")
+    Loading(3)
+    os.system('cls||echo -e \\\\033c')
+    exit()
 
-I = 2
+else:
+    print("Not valid Response, Exiting")
+    Loading(3)
+    os.system('cls||echo -e \\\\033c')
+    exit()
+
+
 print("-_" * 30)
-print("Vereficando se Habilitado")
+print("Vereficando se Ligado")
 print("-_" * 30)
-print("\n")
 
-while I >= 0:
-    print(".", end="")
-    time.sleep(1)
-    I -= 1
+Loading(3)
 
-print("\n")
-
-if Habilitado:
+if Ligado:
     print("-_" * 30)
-    print("Esta Habilitado")
+    print("Esta Ligado")
     print("-_" * 30)
-    print("\n" * 3)
 else:
     print("-_" * 30)
-    print("Não esta Habilitado")
+    print("Não esta Ligado")
     print("-_" * 30)
-    print("\n" * 3)
 
+
+
+Loading(3)
 
 #Numeros
 
 Chegada = 100
 Porcentagem = 0
-if Habilitado:
+if Ligado:
     print("Começando Download", "-_" * 30)
     print("\n")
     while Porcentagem <= Chegada:
@@ -56,10 +78,29 @@ for i in ListGames:
     print("--" * 30)
     time.sleep(1)
 
-#Como Listas são chamadas
+print("Any new Games?")
+NewGame = input("=== ")
 
+
+
+print("\n")
+ListGames.append(NewGame)
+
+print("-_" * 30)
+print(f"{NewGame} Downloaded")
+print("-_" * 30)
+
+
+#Metodos de listas
+#.append("Texto"), Adiciona algo ha lista
+#.remove("Texto"), Remove algo da lista
+#.pop[], Remove e retorna o valor removido
+
+
+
+#Como Listas são chamadas
+#LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 #    [Start:   End:   How
 #LIST[  1:      9:     3 ]
 #print(LIST[1:9:3])
 #Output = 3 6 9
-
